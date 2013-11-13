bc8e299e1ea00f6b73159d8cd0560802ebe71d22
Folletto <folletto@gmail.com>
2013-11-13 01:25:57 +0000

    Session is now shared between Express and Socket.io
    So much win.

--- app.js
+++ app.js
@@ -16,14 +16,25 @@ io.set('log level', 1);
 
 
 
+// ****************************************************************************************************
 // ****** Middleware
+
 // Post data
 app.use(express.bodyParser());
 
 // Sessions
 var SESSION_SECRET = 'chumbawamba';
-app.use(express.cookieParser());
-app.use(express.session({secret: SESSION_SECRET}));
+var EXPRESS_SID_KEY = 'express.sid';
+var cookieParser = express.cookieParser(SESSION_SECRET);
+var sessionStore = new express.session.MemoryStore();
+app.use(cookieParser);
+app.use(express.session({
+  store: sessionStore,
+  cookie: { 
+    httpOnly: true
+  },
+  key: EXPRESS_SID_KEY
+}));
 
 // Jade
 app.engine('jade', require('jade').__express);
@@ -31,12 +42,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 
+
+// ****************************************************************************************************
 // ****** HTTP + Express
 server.listen(3000);
 app.use(express.static(__dirname + "/public"));
 
 
 
+// ****************************************************************************************************
 // ****** Routing
 app.get('/', function(req, res) {
   res.render('index', {
@@ -58,14 +72,56 @@ app.all('/c', function(req, res) {
 });
 
 
+// ****************************************************************************************************
+// ****** Sharing sessions
+//
+// Socket.io Authorization -> https://github.com/LearnBoost/socket.io/wiki/Authorizing
+// Sessions bridge -> https://github.com/leeroybrun/socketio-express-sessions/blob/master/server.js
+// 
+io.set('authorization', function wb_authorization(data, callback) {
+  
+  // Cookies?
+  if (data.headers.cookie) {
+    
+    // Getting into the guts of Express 3.0:
+    // Call the initialized cookieParser(req, res, next) and handle its next() function call.
+    // Express cookieParser(req, res, next) is used initialy to parse data in "req.headers.cookie".
+    // Here our cookies are stored in "data.headers.cookie", so we just pass "data" to the first argument of function
+    cookieParser(data, {}, function(parseErr) {
+      if(parseErr) { return callback('Error parsing cookies.', false); }
+      
+      // Get the SID that has been decoded by cookieParser into data
+      var sessionid = (data.signedCookies && data.signedCookies[EXPRESS_SID_KEY]) ||
+                      (data.cookies && data.cookies[EXPRESS_SID_KEY]);
+      
+      // Now let's load the session
+      sessionStore.load(sessionid, function(err, session) {
+        if (!err) {
+          if (session) {
+            data.session = session; // GAME ON, connecting the Express session. Look: socket.handshake.session
+            callback(null, true); // Required to start the connection. 
+          } else {
+            callback('error: undefined session', false);
+          }
+        } else {
+          callback('error: ' + err, false);
+        }
+      });
+    });
+  }
+});
 
+
+
+// ****************************************************************************************************
 // ****** Socket.io
 // This connects to the client
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function wb_iosocket(socket) {
   
   // Create the connected object.
   // TODO: maybe a pawn manager class?
   var p = new pawn.Pawn(socket);
+  console.info(socket.handshake.session);
   
   socket.on('disconnect', function(data) {
     // TODO: make a softer disconnect with a timeout for reconnection
